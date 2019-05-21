21e217046f2cf7162ba9eb0625fe499fa5847216
Ayushhiman <ayush.himan@gmail.com>
2019-05-21 11:09:39 +0530

    first commit

--- LoR.py
+++ LoR.py
@@ -14,31 +14,48 @@ sns.jointplot(x='Age',y='Area Income',data=ad_data)
 sns.jointplot(x='Age',y='Daily Time Spent on Site',data=ad_data,color='red',kind='kde');
 sns.jointplot(x='Daily Time Spent on Site',y='Daily Internet Usage',data=ad_data,color='green')
 sns.pairplot(ad_data,hue='Clicked on Ad',palette='bwr')
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold
-from sklearn.feature_selection import RFECV
-l= LogisticRegression()
-rfecv = RFECV(estimator=l, step=1, cv=StratifiedKFold(2),
-              scoring='accuracy')
+from sklearn.model_selection import train_test_split
 X = ad_data[['Daily Time Spent on Site', 'Age', 'Area Income','Daily Internet Usage', 'Male']]
 y = ad_data['Clicked on Ad']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+l1 = LogisticRegression()
+l1.fit(X_train,y_train)
+p1 = l1.predict(X_test)
+from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_selection import RFECV
+l2= LogisticRegression()
+rfecv = RFECV(estimator=l2, step=1, cv=StratifiedKFold(2),
+              scoring='accuracy')
 rfecv.fit(X_train, y_train)
-rfecv.transform(X_train)
+print(rfecv.transform(X_train)[:1,:])
+print(X_train.head(1))
+print('By comparing the two we find the feature not selected')
 print('Number of best suited features using RFFECV')
 print(rfecv.n_features_)
-p1=rfecv.predict(X_test)
-logmodel = LogisticRegression()
-logmodel.fit(X_train,y_train)
-predictions = logmodel.predict(X_test)
-
+p2=rfecv.predict(X_test)
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+scaled_data = scaler.transform(X_train)
+from sklearn.decomposition import PCA
+pca = PCA(n_components=4)
+pca.fit(scaled_data)
+xtrain_pca = pca.transform(scaled_data) 
+xtest_pca=pca.transform(scaler.transform(X_test))
+l3=LogisticRegression()
+l3.fit(xtrain_pca,y_train)
+p3 = l3.predict(xtest_pca)
+df_comp = pd.DataFrame(pca.components_,columns=X.columns)
+print('PCA components for the features')
+print(df_comp)
 from sklearn.metrics import classification_report
 print('model 1')
-print(classification_report(y_test,predictions))
-print('model 2')
 print(classification_report(y_test,p1))
-
+print('model 2')
+print(classification_report(y_test,p2))
+print('model 3')
+print(classification_report(y_test,p3))
 
 
 
